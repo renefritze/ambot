c8bbb7c33d0f9755cb07574e5b0370f2bc43fbab
Rene Milk <koshi@springlobby.info>
2009-05-29 02:29:22 +0200

    onthing

--- ambot.py
+++ ambot.py
@@ -7,7 +7,7 @@ from time import *
 
 class PersistentList:
 	filename = ""
-	data = list()
+	data = []
 	def __init__(self,name):
 		self.filename = name + ".list"
 
@@ -49,7 +49,7 @@ class Main:
 				print "error: not enough arguments"
 		
 	def oncommandfromserver(self,command,args,socket):
-	    if command == "SAID" and len(args) > 2:
+	    if command == "ADDUSER" and len(args) > 2:
 			if args[2] == "!faq" and len(args) > 3:
 				now = time()
 				user = args[1]
@@ -71,40 +71,13 @@ class Main:
 				    faqstring += key + " "
 				socket.send("SAY %s %s\n" % (args[0],faqstring ))
 
-	def loadFaqs( self ):
-		faqfile = open(self.filename,'r')
-		content = faqfile.read()
-		entries = content.split('|')
-		i = 0
-		while i < len(entries) - 1  :
-			self.faqs[entries[i]] = entries[i+1]
-			i += 2
-		faqfile.close()
-
-	def saveFaqs( self ):
-		faqfile = open(self.filename,'w')
-		for key,msg in self.faqs.items():
-			faqfile.write( key + "|" + msg + "|" )
-		faqfile.flush()
-		faqfile.close()
-
-	def addFaq( self, key, args ):
-		msg = " "
-		for arg in args :
-			msg +=  arg + " "
-		if msg != "" :
-			msg = msg.replace( "\\n", '\n' )
-			self.faqs[key] = msg
-		self.saveFaqs()
-
 	def ondestroy( self ):
 	    self.saveFaqs()
 
 	def onload(self,tasc):
 	  self.app = tasc.main
-	  self.chans = parselist(self.app.config["channels"],',')
 	  self.admins = parselist(self.app.config["admins"],',')
-	  self.filename = parselist(self.app.config["faqfile"],',')[0]
+	  self.users = parselist(self.app.config["users"],',')
 	  self.loadFaqs()
 
 		
